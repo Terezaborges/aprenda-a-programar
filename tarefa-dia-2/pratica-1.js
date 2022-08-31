f29859a538f9db3849d6115b579345df5ac5eafc
Terezaborges <btereza469@gmail.com>
2022-08-31 19:01:30 -0300

    Arquivo 1 atualizado

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,13 @@
+let nome = "Tereza Stefany Borges"
+let email = "btereza469@gmail.com"
+let profissao = "Jovem aprendiz"
+let idade = "19"
+let lugar = "Venezuela"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+
+console.log("")
